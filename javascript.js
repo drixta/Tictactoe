bb111fd4f7a5a150527945dbae6c153a1fcac9e1
unknown <drixta@gmail.com>
2013-04-06 22:17:00 -0700

    Added highlight on boxes and select square using id

--- javascript.js
+++ javascript.js
@@ -1,60 +1,70 @@
-window.onload = function(){
-var canvas_len = 500;
+$(document). ready(function(){
+var canvasLen = 500;//size of the desk
+var dimension = 10;
 var stage = new Kinetic.Stage({
 	container: 'container',
-	width: canvas_len,
-	height: canvas_len
+	width: canvasLen,
+	height: canvasLen
 });
-var layer = new Kinetic.Layer();
+var stageWidth = stage.getWidth();
+
+
+layer = new Kinetic.Layer({
+});
+
 
 var desk = new Kinetic.Rect({
- 	x: 0,
+	x: 0,
     y: 0,
-    width: canvas_len,
-    height: canvas_len,
+    width: canvasLen,
+    height: canvasLen,
     stroke: 'black',
     strokeWidth: 4
 });
-
-// add the shape to the layer
 layer.add(desk);
 
-//creating Square group
-var square_group = new Kinetic.Group({
-	x: 0,
-	y: 0,
-});
-//adding squares onto layer
-var stage_width = stage.getWidth()
-var dimension = 20;
-var creat_square = function(posr,posc,state,len){
-	var square = new Kinetic.Rect({
-		id : String(posr)+',' + String(posc),
-		posr: posr,
-		posc: posc,
-		state : state,
-		x: posr*len,
-		y: posc*len,
-		width: len,
-		height: len,
-		stroke: 'black',
-		strokeWidth: 2
-	});	
-	square_group.add(square);
-}
-var addsquare = function(dimension){
-	for (var n = 0; n<dimension; n++) {
-		for (var i = 0; i < dimension; i++){
-			var len = stage_width/dimension;
-			creat_square(n,i,0,len);
-		}
-	}
-};
-var highlight = function(square){
-	square.fill = "#00D2FF"
-};
 
-addsquare(10);
-layer.add(square_group);
+var squareGroup = new Kinetic.Group({
+
+});
+
+var state = 0; //neutral squares
+for (var row = 0; row < dimension; row++) {
+	for (var col = 0; col < dimension; col++){
+		var len = stageWidth/dimension;
+		var square = new Kinetic.Rect({
+			id : row + "," + col,
+			name: 'square',
+			rowNumber: row,
+			colNumber: col,
+			state : state,
+			x: row*len,
+			y: col*len,
+			width: len,
+			height: len,
+			stroke: 'black',
+			strokeWidth: 2,
+			fill: 'white'
+		});
+		squareGroup.add(square);
+		console.log(square.attrs.id);
+
+	}
+}
+shape = squareGroup.get('.square');
+single = squareGroup.get('#0,0')[0];
+shape.on("mouseenter", function(){
+	this.setFill('blue');
+	layer.draw();
+});
+shape.on("mouseleave", function(){
+	this.setFill('white');
+	layer.draw();
+});
+shape.each(function(shape){
+	console.log(shape.attrs.id);
+})
+console.log(single.attrs.id);
+layer.add(squareGroup);
 stage.add(layer);
-};
+});
